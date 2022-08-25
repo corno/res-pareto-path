15361d8cb2a1701dbc985340115dba666e14dc67
corno <corno.astn.a@schraverus.com>
2022-08-25 16:12:12 +0200

    .

--- pub/src/imp/index.ts
+++ pub/src/imp/index.ts
@@ -1,11 +1,2 @@
+
 export * from "./parseFilePath"
-import * as api from "api-pareto-path"
-import { parseFilePath } from "./parseFilePath"
-
-type API = {
-    parseFilePath: api.ParseFilePath,
-}
-
-export const $: API = {
-    parseFilePath: parseFilePath
-}
