5f3d4d0c39972c49d6279cec0366ec19f5288610
corno <corno.astn.a@schraverus.com>
2023-01-24 21:05:42 +0100

    .

--- pareto/src/data/api.p.ts
+++ pareto/src/data/api.p.ts
@@ -28,6 +28,7 @@ export const $: mmoduleDefinition.TModuleDefinition = {
             "common": "glo-pareto-common",
             // "main": "glo-pareto-main",
         }),
+        'parameters': d({}),
         'namespace': {
             'types': types({
                 "ParseFilePathData": group({
