c5244f7e044228bff847b91e990ec8c82990f97c
corno <corno.astn.a@schraverus.com>
2023-04-11 15:38:14 +0200

    .

--- typescript/pub/src/implementation.generated.ts
+++ typescript/pub/src/implementation.generated.ts
@@ -4,7 +4,7 @@ import { $$ as iconvertWindowsPathToPosixPath } from "./implementations/convertW
 import { $$ as idirname } from "./implementations/dirname.native"
 import { $$ as iparseFilePath } from "./implementations/parseFilePath.native"
 
-export const $r: API = {
+export const $api: API = {
     'basename': ibasename,
     'convertWindowsPathToPosixPath': iconvertWindowsPathToPosixPath,
     'dirname': idirname,
